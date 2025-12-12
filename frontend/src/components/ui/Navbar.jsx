889523c6b6afdf132f430a2ac2fdb006e2ba2940
Bukari Máté <matebukari@gmail.com>
2025-12-12 22:26:08 +0100

    Improve nav animation

--- frontend/src/components/ui/Navbar.jsx
+++ frontend/src/components/ui/Navbar.jsx
@@ -14,11 +14,11 @@ export default function Navbar() {
   return (
     <nav
       className={`
-        fixed top-2 left-1/2 transform -translate-x-1/2 
+        fixed top-2 left-1/2 transform -translate-x-1/2
         z-20 rounded-xl overflow-hidden 
         bg-black/10 backdrop-blur-[3px] border border-neutral-400/20
         transition-all duration-300
-        ${scrolled ? "w-[calc(100%-40rem)]" : "w-[calc(100%-10rem)]"}
+        ${scrolled ? "w-3/5" : "w-5/6"}
       `}
     >
       <div className="relative w-full px-6 h-16 flex items-center justify-center">
